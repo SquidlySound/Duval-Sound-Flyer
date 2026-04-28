f0a716eeeff23782f5e561b6b86b682bf7a23e47
SquidlySound <31514918+SquidlySound@users.noreply.github.com>
2026-04-28 19:20:27 -0400

    Create admin.js file with admin panel functions

---
+++ admin.js
@@ -0,0 +1,33 @@
+// Admin panel functions
+
+function renderAdminStats() {
+    // Implementation goes here
+}
+
+function renderChart() {
+    // Implementation goes here
+}
+
+function renderActivity() {
+    // Implementation goes here
+}
+
+function renderPasswordList() {
+    // Implementation goes here
+}
+
+function addPassword(password) {
+    // Implementation goes here
+}
+
+function deletePassword(passwordId) {
+    // Implementation goes here
+}
+
+function handleFlyerUpload(file) {
+    // Implementation goes here
+}
+
+function applyNewFlyer(flyerId) {
+    // Implementation goes here
+}
